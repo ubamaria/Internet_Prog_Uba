2cd3ee369498c2d865cadd8931be438d744e5a87
Maria <55685833+ubamaria@users.noreply.github.com>
2020-12-24 22:28:11 +0400

    fixed 5 laba

--- site.js
+++ site.js
@@ -1,54 +1,55 @@
-const form = document.forms[0];
+const form = document.forms['form_order'];
 
 form.addEventListener('submit', (event) => {
-event.preventDefault();
-const values = [...new FormData(form).values()];
-console.log(values);
+    event.preventDefault();
+    const formData = new FormData(form);
+    console.log(...formData.values());
 
-if (!checkIfNotNull(values[0])) return showMessage('error', 'Введите ваше имя');
-if (!checkPhone(values[1])) return showMessage('error', 'Неверно введен номер телефона');
-if (!checkEmail(values[2])) return showMessage('error', 'Неверно введена почта');
-if (!checkCount(values[3])) return showMessage('error', 'Неверно введено количество приборов');
-if (!checkIfNotNull(values[4])) return showMessage('error', 'Введите ваш адрес');
-if (!checkPayment(values[5])) return showMessage('error', 'Выберите способ оплаты');
+        if (!checkIfNotNull(formData.get('FIO'))) return showMessage('error', 'Введите ваше имя');
+        if (!checkPhone(formData.get('phone'))) return showMessage('error', 'Неверно введен номер телефона');
+        if (!checkEmail(formData.get('email'))) return showMessage('error', 'Неверно введена почта');
+        if (!checkCount(formData.get('count'))) return showMessage('error', 'Неверно введено количество приборов');
+        if (!checkIfNotNull(formData.get('address'))) return showMessage('error', 'Введите ваш адрес');
+        if (!checkPayment(formData.get('payment'))) return showMessage('error', 'Выберите способ оплаты');
 
-showMessage('success', 'Заказ оформлен');
-})
+        showMessage('success', 'Заказ оформлен');
+    });
 
-function checkIfNotNull(str) {
-return str !== '';
-}
+    function checkIfNotNull(str) {
+        return str !== '';
+    }
 
-function checkPhone(phone) {
-var reg = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
-return phone != "" && reg.test(phone);
-}
-function checkEmail(email) {
-    var reg = /^([A-Za-z0-9-.])+@([A-Za-z0-9-.])+.([A-Za-z]{2,4})$/;
-    return email != "" && reg.test(email);
-}
-function checkCount(count) {
-    if (count>=0){
-    return count != ""
-    };
-}
-function checkPayment(payment) {
-    var inp = document.getElementsByName('payment');
+    function checkPhone(phone) {
+        var reg = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+        return phone != "" && reg.test(phone);
+    }
+    function checkEmail(email) {
+        var reg = /^([A-Za-z0-9-.])+@([A-Za-z0-9-.])+.([A-Za-z]{2,4})$/;
+        return email != "" && reg.test(email);
+    }
+    function checkCount(count) {
+        if (count >= 0) {
+            return count != ""
+        };
+        return;
+    }
+    function checkPayment(payment) {
+        var inp = document.getElementsByName('payment');
 
-    for (var i = 0; i < inp.length; i++) {
-      var radio = inp[i];
-      if (radio.type == "radio" && radio.checked) {
-          return true
-      }
-      }
-      return false;
-  }
-function showMessage(type, text) {
-Swal.fire({
-position: 'center',
-text: text,
-icon: type,
-showConfirmButton: false,
-timer: 1500
-})
-}
+        for (var i = 0; i < inp.length; i++) {
+            var radio = inp[i];
+            if (radio.type == "radio" && radio.checked) {
+                return payment;
+            }
+        }
+        return;
+    }
+    function showMessage(type, text) {
+        Swal.fire({
+            position: 'center',
+            text: text,
+            icon: type,
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
